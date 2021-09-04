00d491fa05d95cae53bc3158f44d1a104538e6b1
Diogo Macedo <diogobr@gmail.com>
2021-09-04 17:46:57 -0300

    Adicionado examplo de teste unitário parametrizado.

--- src/main/java/br/com/utilitarios/Anagrama.java
+++ src/main/java/br/com/utilitarios/Anagrama.java
@@ -4,53 +4,56 @@ import java.util.HashMap;
 
 public class Anagrama {
 
+	HashMap<String, Integer> mapPalavra1 = new HashMap<String, Integer>();
+	HashMap<String, Integer> mapPalavra2 = new HashMap<String, Integer>();
+
 	public boolean isAnagrama(String palavra1, String palavra2) {
 
-		boolean result = true;
 
-		int tamanhoPalavra1 = palavra1.length();
+		if (palavra1.length() != palavra2.length()) {
+			return false;
+		} else {
 
-		HashMap<String, Integer> letrasQtdeString1 = new HashMap<String, Integer>();
-		HashMap<String, Integer> letrasQtdeString2 = new HashMap<String, Integer>();
+			for (char letra : palavra1.toCharArray()) {
+				this.atualizarQtdeLetra(mapPalavra1, letra);
+			}
 
-		int qtdeStr1 = 0;
-		int qtdeStr2 = 0;
-
-		for (char letra : palavra1.toCharArray()) {
-
-			if (palavra1.length() != palavra2.length()) {
-				result = false;
-				break;
-			} else if (palavra2.indexOf(letra) >= 0) {
-
-				String strLetra = String.valueOf(letra);
-
-				Integer qtdeStr01 = letrasQtdeString1.get(strLetra);
-				qtdeStr1 = qtdeStr01.intValue();
-				qtdeStr1++;
-				letrasQtdeString1.put(strLetra, qtdeStr1);
-
-				Integer qtdeStr02 = letrasQtdeString2.get(strLetra);
-				qtdeStr2 = qtdeStr02.intValue();
-				qtdeStr2++;
-				letrasQtdeString2.put(strLetra, qtdeStr2);
-
+			for (char letra : palavra2.toCharArray()) {
+				this.atualizarQtdeLetra(mapPalavra2, letra);
 			}
 
 		}
 
 		for (char letra : palavra1.toCharArray()) {
-			String strLetra = String.valueOf(letra);
-			Integer qtdeStr01 = letrasQtdeString1.get(strLetra);
-			Integer qtdeStr02 = letrasQtdeString2.get(strLetra);
-			if (qtdeStr01.intValue() != qtdeStr02.intValue()) {
-				result = false;
-				break;
+			int qtdeLetraPalavra01 = this.obterQtdeLetra(mapPalavra1, letra);
+			int qtdeLetraPalavra02 = this.obterQtdeLetra(mapPalavra2, letra);
+			if (qtdeLetraPalavra01 != qtdeLetraPalavra02) {
+				return false;
 			}
 		}
 
-		return false;
+		return true;
 
 	}
 
+	private void atualizarQtdeLetra(HashMap<String, Integer> hashMap, char letra) {
+		String strLetra = String.valueOf(letra);
+		int qtdeLetra = this.obterQtdeLetra(hashMap, strLetra) + 1;
+		hashMap.put(strLetra, qtdeLetra);
+	}
+
+	private int obterQtdeLetra(HashMap<String, Integer> hashMap, String letra) {
+		if (hashMap.containsKey(letra)) {
+			return hashMap.get(letra);
+		}
+		return 0;
+	}
+
+	private int obterQtdeLetra(HashMap<String, Integer> hashMap, char letra) {
+		String strLetra = String.valueOf(letra);
+		return this.obterQtdeLetra(hashMap, strLetra);
+	}
+
 }
+
+
--- src/test/java/br/com/utilitarios/AnagramaTest.java
+++ src/test/java/br/com/utilitarios/AnagramaTest.java
@@ -1,16 +1,18 @@
 package br.com.utilitarios;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class AnagramaTest {
 
 	private Anagrama anagrama;
 
-	@BeforeAll
-	void beforeAllTests() {
+	@BeforeEach
+	void init() {
 		this.anagrama = new Anagrama();
 	}
 
@@ -23,6 +25,21 @@ public class AnagramaTest {
 		boolean resultado = this.anagrama.isAnagrama(palavra1, palavra2);
 
 		assertEquals(true, resultado);
+
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"ramo, roma, true",
+		"podendo, depondo, true",
+		"pato, pata, false",
+		"aaab, abbb, false"
+	})
+	void testPalavras(String palavra1, String palavra2, boolean resultadoEsperado) {
+
+		boolean resultado = this.anagrama.isAnagrama(palavra1, palavra2);
+
+		assertEquals(resultadoEsperado, resultado);
 
 	}
 
