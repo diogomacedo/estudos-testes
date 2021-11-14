d55507a9a1131a1e339ddc9a7a4185a3edefb493
Diogo Macedo <diogobr@gmail.com>
2021-11-14 13:57:53 -0300

    Adicionados métodos para serem executados durante a criação e destruição da instância da classe de teste; e antes e depois da execução de cada teste.

--- src/test/java/br/com/curso/servicos/LocacaoServiceTest.java
+++ src/test/java/br/com/curso/servicos/LocacaoServiceTest.java
@@ -2,7 +2,11 @@ package br.com.curso.servicos;
 
 import java.util.Date;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.curso.entidades.Filme;
@@ -12,8 +16,30 @@ import br.com.curso.utils.DateUtils;
 
 public class LocacaoServiceTest {
 
+	@BeforeAll
+	public static void setupAll() {
+		System.out.println("Teste before all");
+	}
+
+	@AfterAll
+	public static void tearDownAll() {
+		System.out.println("Teste after all");
+	}
+
+	@BeforeEach
+	public void setup() {
+		System.out.println("Teste before each");
+	}
+
+	@AfterEach
+	public void tearDown() {
+		System.out.println("Teste after each");
+	}
+
 	@Test
 	public void testeLocacao() throws Exception {
+
+		System.out.println("testeLocacao()");
 
 		// Cenário
 		Usuario usuario = new Usuario("Diogo Macedo");
@@ -33,6 +59,8 @@ public class LocacaoServiceTest {
 	@Test
 	public void testeLocacao_SemEstoque1() {
 
+		System.out.println("Teste Locacao Sem Estoque 1");
+
 		// Cenário
 		Usuario usuario = new Usuario("Diogo Macedo");
 		Filme filme = new Filme("Star Wars - Ataque dos Clones", 0, 5.50);
@@ -46,6 +74,8 @@ public class LocacaoServiceTest {
 
 	@Test
 	public void testeLocacao_SemEstoque2() {
+
+		System.out.println("Teste Locacao Sem Estoque 2");
 
 		// Cenário
 		Usuario usuario = new Usuario("Diogo Macedo");
